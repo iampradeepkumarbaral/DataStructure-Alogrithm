f5087b8fd0f3e50096e2a5aa03c266a2fc8c9fa7
Your Name <you@example.com>
2020-12-29 12:41:38 +0530

    Double Lindelist

---
+++ dart/LinkedList/DynamicInsertLinklist.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main(){
+
+ /* stdout.write("Enter your name : ");
+  var name = stdin.readLineSync();
+  stdout.write(name);*/
+  	stdout.write("Enter The Size Of An Array: ");
+	var size = stdin.readLineSync();
+  	//stdout.write(size);
+  	int sizevalue = int.parse(size);
+  	print(sizevalue);
+	
+}
